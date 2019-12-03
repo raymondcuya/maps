c5ac74b40e39dc768dda37f5d371301da1c4fbd0
raymondcuya <raymondcuya@gmail.com>
2019-12-03 20:10:25 +1100

    calling maps

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,8 @@ console.log(user);
 
 const company = new Company();
 console.log(company);
+
+const map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: { lat: 0, lng: 0 }
+});
